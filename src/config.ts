2582a4396a9de3869c7424353f2d5ecc3e0ad6a8
Shaheerhashmi11 <shaheer_hashmi@hotmail.com>
2026-06-04 23:57:22 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,6 @@
 export const SUBTITLE = "Flawed Visuals"
 export const TITLE = "Content Creator & Visual Artiste"
 export const TAGLINE = "MEMORIES | CULTURE | STORIES"
-
 // Contacts
 export const contact = "Keep in touch"
 export const email = "hello@flawedvisuals.com"
